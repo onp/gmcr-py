a569ef666ef6229609117de8a7784c9102608e2c
Oskar Petersons <onptrs@gmail.com>
2017-02-14 20:40:22 -0500

    cleanup

--- visualizerLauncher.py
+++ visualizerLauncher.py
@@ -17,23 +17,23 @@ def serve():
     # required to successfully freeze multiprocessing to a win32gui application
     sys.stderr = Fakestd()
     sys.stdout = Fakestd()
-    
-    #move to the temp folder where the visualizer files were copied
+
+    # move to the temp folder where the visualizer files were copied
     os.chdir(os.environ['TEMP'] + '/gmcr-vis')
 
     PORT = 8000
     Handler = http.server.SimpleHTTPRequestHandler
-    httpd = http.server.HTTPServer(("",PORT),Handler)
+    httpd = http.server.HTTPServer(("", PORT), Handler)
     print("now serving on port ", PORT)
     httpd.serve_forever()
 
-    
-    
+
 def launchVis(data=None):
-    #serverThread = Process(target=serve,daemon=True)   #daemon keyword only works in python 3.3 and up
+    # serverThread = Process(target=serve,daemon=True)
+    # daemon keyword only works in python 3.3 and up
     serverThread = Process(target=serve)
     serverThread.daemon = True
-    
+
     serverThread.start()
 
-    webbrowser.open("http://127.0.0.1:8000",2,True)
+    webbrowser.open("http://127.0.0.1:8000", 2, True)
