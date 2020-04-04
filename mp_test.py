502a12c9100962aaa0551763b74d303864967b80
onp <onptrs@gmail.com>
2020-04-04 10:13:01 -0400

    linting

--- mp_test.py
+++ mp_test.py
@@ -1,35 +1,33 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import Tk, StringVar
+from tkinter import ttk as ttk
 from subprocess import Popen, PIPE
 import sys
 import multiprocessing as mp
 
+
 class MainWindow:
 
     def __init__(self):
-    
+
         self.root = Tk()
         self.labelVar = StringVar()
         self.labelVar.set("start")
-        self.label = ttk.Label(self.root,textvariable = self.labelVar )
-        
-        self.label.grid(column=0,row=0)
-        
+        self.label = ttk.Label(self.root, textvariable=self.labelVar)
+
+        self.label.grid(column=0, row=0)
+
         self.root.mainloop()
 
-        
-        
+
 def initApp():
     app = MainWindow()
-    
+
+
 def runProc():
     p = mp.Process(target=initApp)
     p.start()
-    
+
 
 if __name__ == '__main__':
     runProc()
     self.process = Popen([sys.executable], stdout=PIPE)
-
-    
